adc453300c78da878d1437267badbbd51a02d6b0
fora-heims <foraheims@gmail.com>
2021-09-29 15:50:54 -0700

    animal sounds now play when you click on the corresponding images

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
-const horseImg = document.getElementById('horse-img')
+const horseImg = document.getElementById('horse-img');
+const dogImg = document.getElementById('dog-img');
+const catImg = document.getElementById('cat-img');
+
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+horseImg.addEventListener('click', () => {
+    horseSound.play();
+});
+
+dogImg.addEventListener('click', () => {
+    dogSound.play();
+});
+
+catImg.addEventListener('click', () => {
+    catSound.play();
+});
