ab7cdc1216110caa3181ca857230360fd4fac3e3
fora-heims <foraheims@gmail.com>
2021-09-29 17:10:15 -0700

    connected animation to event click on animal or press key.

--- app.js
+++ app.js
@@ -8,24 +8,54 @@ const dogSound = document.getElementById('dog-sound');
 const catSound = document.getElementById('cat-sound');
 // initialize global state
 
+// function removeClass() {
+//     horseImg.classList.remove('animate-animal');
+//     dogImg.classList.remove('animate-animal');
+//     catImg.classList.remove('animate-animal');
+// }
+
 horseImg.addEventListener('click', () => {
     horseSound.play();
+    horseImg.classList.remove('animate-animal');
+    dogImg.classList.remove('animate-animal');
+    catImg.classList.remove('animate-animal');
+    horseImg.classList.add('animate-animal');
 });
 
 dogImg.addEventListener('click', () => {
     dogSound.play();
+    horseImg.classList.remove('animate-animal');
+    dogImg.classList.remove('animate-animal');
+    catImg.classList.remove('animate-animal');
+    dogImg.classList.add('animate-animal');
 });
 
 catImg.addEventListener('click', () => {
     catSound.play();
+    horseImg.classList.remove('animate-animal');
+    dogImg.classList.remove('animate-animal');
+    catImg.classList.remove('animate-animal');
+    catImg.classList.add('animate-animal');
 });
 
 document.addEventListener('keyup', event => {
     if (event.key === 'c') {
         catSound.play();
+        horseImg.classList.remove('animate-animal');
+        dogImg.classList.remove('animate-animal');
+        catImg.classList.remove('animate-animal');
+        catImg.classList.add('animate-animal');
     } else if (event.key === 'd') {
         dogSound.play();
+        horseImg.classList.remove('animate-animal');
+        dogImg.classList.remove('animate-animal');
+        catImg.classList.remove('animate-animal');
+        dogImg.classList.add('animate-animal');
     } else if (event.key === 'h') {
         horseSound.play();
+        horseImg.classList.remove('animate-animal');
+        dogImg.classList.remove('animate-animal');
+        catImg.classList.remove('animate-animal');
+        horseImg.classList.add('animate-animal');
     }
 });
