3a8651b6d441daa9be0f685a2656174237bfa512
fora-heims <foraheims@gmail.com>
2021-09-29 19:51:38 -0700

    Finally made the animation able to be activated on the same animaltwice in a row by using setTimeout

--- app.js
+++ app.js
@@ -16,46 +16,46 @@ const catSound = document.getElementById('cat-sound');
 
 horseImg.addEventListener('click', () => {
     horseSound.play();
-    horseImg.classList.remove('animate-animal');
-    dogImg.classList.remove('animate-animal');
-    catImg.classList.remove('animate-animal');
     horseImg.classList.add('animate-animal');
+    setTimeout(function() {
+        horseImg.classList.remove('animate-animal');
+    }, 800);
 });
 
 dogImg.addEventListener('click', () => {
     dogSound.play();
-    horseImg.classList.remove('animate-animal');
-    dogImg.classList.remove('animate-animal');
-    catImg.classList.remove('animate-animal');
     dogImg.classList.add('animate-animal');
+    setTimeout(function() {
+        dogImg.classList.remove('animate-animal');
+    }, 800);
 });
 
 catImg.addEventListener('click', () => {
     catSound.play();
-    horseImg.classList.remove('animate-animal');
-    dogImg.classList.remove('animate-animal');
-    catImg.classList.remove('animate-animal');
     catImg.classList.add('animate-animal');
+    setTimeout(function() {
+        catImg.classList.remove('animate-animal');
+    }, 800);
 });
 
 document.addEventListener('keyup', event => {
     if (event.key === 'c') {
         catSound.play();
-        horseImg.classList.remove('animate-animal');
-        dogImg.classList.remove('animate-animal');
-        catImg.classList.remove('animate-animal');
         catImg.classList.add('animate-animal');
+        setTimeout(function() {
+            catImg.classList.remove('animate-animal');
+        }, 800);
     } else if (event.key === 'd') {
         dogSound.play();
-        horseImg.classList.remove('animate-animal');
-        dogImg.classList.remove('animate-animal');
-        catImg.classList.remove('animate-animal');
         dogImg.classList.add('animate-animal');
+        setTimeout(function() {
+            dogImg.classList.remove('animate-animal');
+        }, 800);
     } else if (event.key === 'h') {
         horseSound.play();
-        horseImg.classList.remove('animate-animal');
-        dogImg.classList.remove('animate-animal');
-        catImg.classList.remove('animate-animal');
         horseImg.classList.add('animate-animal');
+        setTimeout(function() {
+            horseImg.classList.remove('animate-animal');
+        }, 800);
     }
 });
